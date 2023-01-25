9090ab30ed5d7b2993d15832abac744d4aef28bc
Tohir Raihan <tohir.raihan@gmail.com>
2023-01-25 13:12:33 +0600

    impliment leaflet to the project

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+	navigator.geolocation.getCurrentPosition(
+		// Success callback function
+		position => {
+			const { latitude, longitude } = position.coords;
+
+			const map = L.map('map').setView([latitude, longitude], 13);
+
+			L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+				attribution:
+					'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+			}).addTo(map);
+
+			L.marker([latitude, longitude])
+				.addTo(map)
+				.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+				.openPopup();
+		},
+
+		// Error callback function
+		() => {
+			alert('Could not get your location!');
+		}
+	);
+}
